418b41d7c41a9c8f5fb6cbbf87ca1bfdb4f5e94d
roodbaraky <kooroshr@hotmail.com>
2024-06-14 13:29:20 +0100

    implemented saving basket contents to localStorage on refresh / navigate away (bit slow atm)

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -2,48 +2,67 @@
 import { BasketContext } from "@/context/context";
 import { basketCounter, basketTotaller } from "@/utils/utils";
 import { ShoppingBasket } from "lucide-react";
-import { useContext, useState } from "react";
+import { usePathname } from "next/navigation";
+import { useCallback, useContext, useEffect, useState } from "react";
+import CheckoutButton from "./CheckoutButton";
 import ItemTile, { BasketItem } from "./ItemTile";
 import { Button } from "./ui/button";
-import CheckoutButton from "./CheckoutButton";
+
 export default function Basket() {
   const [isBasketOpen, setIsBasketOpen] = useState(false);
-  const { basket } = useContext(BasketContext);
+  const { basket, setBasket } = useContext(BasketContext);
+  const pathname = usePathname();
   const handleBasketClick = () => {
     setIsBasketOpen(!isBasketOpen);
   };
-  const basketCount = basketCounter(basket)
-
+  const saveBasketBeforeExit = useCallback(() => {
+    if (basket.length) {
+      localStorage.clear();
+      localStorage.setItem("basket", JSON.stringify(basket));
+    }
+  },[basket]);
+  const basketCount = basketCounter(basket);
+  if (typeof window !== "undefined") {
+    window.addEventListener("beforeunload", saveBasketBeforeExit);
+  }
+  useEffect(() => {
+    saveBasketBeforeExit();
+  }, [pathname, saveBasketBeforeExit]);
   return (
-    <section className="top-18 fixed right-5 custom-right" >
+    <section className="top-18 fixed right-5 custom-right">
       <Button onClick={handleBasketClick}>
         {basketCount} <ShoppingBasket />
       </Button>
-      {basketCount===0
-      ?(<section
-      id="basket-empty-msg"
-      className={`transition-all delay-200 ease-in-out fixed top-36 right-5 custom-right p-2 m-2 bg-slate-400 w-64 rounded-xl max-h-[80%] overflow-scroll ${
-        isBasketOpen ? "visible" : "hidden"
-      }`}
-    >no items!</section>  )    
-      :(<section
-        id="basket-container"
-        className={`transition-all delay-200 ease-in-out fixed top-36 right-5 custom-right p-2 m-2 bg-slate-400 w-64 rounded-xl max-h-[80%] overflow-scroll ${
-          isBasketOpen ? "visible" : "hidden"
-        }`}
-      >
-        {basket?.map((item: BasketItem) => (
-          <ItemTile key={item.id} item={item} />
-        ))}
-        <div className="flex flex-col  bottom-0 bg-slate-600 rounded-xl w-full px-2 pb-2 text-white italic">
-          Subtotal:
-          <div className="flex justify-between">
-            ₽{basketTotaller(basket, "poke")}
-            <div>£{basketTotaller(basket, "gbp")}</div>
+      {basketCount === 0 ? (
+        <section
+          id="basket-empty-msg"
+          className={`transition-all delay-200 ease-in-out fixed top-36 right-5 custom-right p-2 m-2 bg-slate-400 w-64 rounded-xl max-h-[80%] overflow-scroll ${
+            isBasketOpen ? "visible" : "hidden"
+          }`}
+        >
+          no items!
+        </section>
+      ) : (
+        <section
+          id="basket-container"
+          className={`transition-all delay-200 ease-in-out fixed top-36 right-5 custom-right p-2 pb-0 m-2 bg-slate-400 w-64 rounded-xl max-h-[80%] overflow-scroll  ${
+            isBasketOpen ? "visible" : "hidden"
+          }`}
+        >
+          {basket?.map((item: BasketItem) => (
+            <ItemTile key={item.id} item={item} />
+          ))}
+          <div className="flex flex-col sticky bottom-0 bg-slate-600 rounded-xl w-full px-2 pb-2 text-white italic">
+            Subtotal:
+            <div className="flex justify-between">
+              ₽{basketTotaller(basket, "poke")}
+              <div>£{basketTotaller(basket, "gbp")}</div>
+            </div>
+            {/* put a ternary here for an alt Checkout if already logged in */}
+            <CheckoutButton />
           </div>
-          <CheckoutButton/>
-        </div>
-      </section>)}
+        </section>
+      )}
     </section>
   );
 }
--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { BasketItem } from "@/components/ItemTile";
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const BasketContext = createContext<{
   basket: BasketItem[];
@@ -12,11 +12,16 @@ export const BasketContext = createContext<{
 
 export function BasketWrapper({ children }: { children: React.ReactNode }) {
   const [basket, setBasket] = useState<BasketItem[]>([]);
-
+  useEffect(() => {
+    const storedBasket = localStorage.getItem("basket");
+    if (storedBasket) {
+      setBasket(JSON.parse(storedBasket));
+    }
+  }, []);
+  //
   return (
     <BasketContext.Provider value={{ basket, setBasket }}>
       {children}
     </BasketContext.Provider>
   );
 }
-
